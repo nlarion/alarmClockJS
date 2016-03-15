5a55e723c40b4f9214334216321a67d949fe3391
Michael Smith and Neil Larion <michaelanthonysmith00@gmail.com>
2016-03-15 10:50:39 -0700

    display text telling user to wake up when the alarm goes off

--- js/time-interface.js
+++ js/time-interface.js
@@ -7,5 +7,24 @@ var runTime = function(){
   setTimeout(function () {
       $('#time').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
       runTime();
+      var globalTime = moment().format('h:mm');
+      var alarms = $(".singleAlarm");
+      console.log(alarms);
+      for (var i = 0; i < alarms.length; i++) {
+        var singleAlarmTime = $(alarms[i]).attr("time");
+        if(globalTime >= singleAlarmTime){
+          $(alarms[i]).html(singleAlarmTime+" WAKE UP!");
+
+        }
+      }
     }, 1000);
 }
+
+$(document).ready(function(){
+  $('#alarmTime').submit(function(event){
+    event.preventDefault();
+    var userTime = $('#alarm').val();
+    $(".userAlarm").prepend("<div class='singleAlarm' time="+userTime+">Alarm Set For: "+userTime+"</div>");
+
+  });
+});
